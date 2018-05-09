dd271f4aeb7d4a1ac297af730f9a7f97123759f3
Kenia <keniavalladarez@gmail.com>
2018-05-09 11:38:03 -0400

    Import needed componenets into App file.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import MessageInput from "./components/MessageInput";
+import MessageView from "./components/MessageView";
 
 class App extends Component {
+  componentDidMount() {
+    this.props.messages.subscribe(() => this.forceUpdate());
+  }
+
   render() {
+    const messages = this.props.messages;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="ui segment">
+        <MessageView messages={messages} />
+        <MessageInput />
       </div>
     );
   }
