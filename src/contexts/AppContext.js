403c968dae61f5eb72fab2d7f5df469694850955
Kenia <keniavalladarez@gmail.com>
2018-05-09 12:17:34 -0400

    Add addMessage and deleteMessage actions to AppContext.

--- src/contexts/AppContext.js
+++ src/contexts/AppContext.js
@@ -7,9 +7,30 @@ export class AppProvider extends React.Component {
     messages: []
   };
 
+  addMessage = message => {
+    this.setState({
+      messages: state.messages.concat(message)
+    });
+  };
+
+  deleteMssage = index => {
+    return {
+      messages: [
+        ...state.messages.slice(0, index),
+        ...state.messages.slice(index + 1, state.messages.length)
+      ]
+    };
+  };
+
   render() {
     return (
-      <AppContext.Provider value={{ ...this.state }}>
+      <AppContext.Provider
+        value={{
+          ...this.state,
+          addMessage: this.addMessage,
+          deleteMssage: this.deleteMssage
+        }}
+      >
         {this.props.children}
       </AppContext.Provider>
     );
