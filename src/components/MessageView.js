91db4c69d19a668c22cfc6f334c3ccee3f2afb56
Kenia <keniavalladarez@gmail.com>
2018-05-10 10:30:31 -0400

    Remove console.log and use Context conusmer to get state and action.

--- src/components/MessageView.js
+++ src/components/MessageView.js
@@ -1,17 +1,24 @@
 import React from "react";
+import { AppContext } from "../contexts/AppContext";
 
 class MessageView extends React.Component {
   render() {
-    const messages = this.props.messages.map((message, index) => (
-      <div
-        className="comment"
-        key={index}
-        onClick={() => this.props.deleteMessage(index)}
-      >
-        {message}
-      </div>
-    ));
-    console.log(messages);
+    const messages = (
+      <AppContext.Consumer>
+        {({ messages, deleteMessage }) =>
+          messages.map((message, index) => (
+            <div
+              className="comment"
+              key={index}
+              onClick={() => deleteMessage(index)}
+            >
+              {message}
+            </div>
+          ))
+        }
+      </AppContext.Consumer>
+    );
+
     return (
       <div className="ui center aligned basic segment">
         <div className="ui comments">{messages}</div>
